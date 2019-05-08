dddbb14406ff512658b2fa4a5a62ddb92fa90de6
igorbragaia <igorbragaia@gmail.com>
2019-05-08 10:53:05 -0300

    first part of lab3 done

--- labs/lab3/script.js
+++ labs/lab3/script.js
@@ -1,34 +1,39 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer({
-    antialias: true
-});
+let scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x220000);
 document.body.appendChild(renderer.domElement);
 
-var slots = []
-var pieces = []
+let mat_clear = new THREE.MeshLambertMaterial( { color: 0xFF7F7F} ),
+    mat_dark = new THREE.MeshLambertMaterial( { color: 0xCE2029} ),
+    mat_highlight = new THREE.MeshBasicMaterial( {color:0xFFC0CB} );
+
 const params = {
   height: 0.2,
   max_radius: 1,
   min_radius: 0.5,
   n: 10,
 }
-var state = {
-  bars:[[],[],[]],
-}
 
-var subscribeCylinders = function(bar){
-  const step=(params.max_radius-params.min_radius)/params.n;
+let slots = [],
+    state = {
+      bars:[[],[],[]],
+    };
+
+let subscribeCylinders = function(bar){
+  const step = (params.max_radius-params.min_radius)/params.n;
   for(let i=1; i<=params.n; i++)
     subscribeCylinder(params.max_radius-i*step, bar);
 }
 
-var subscribeCylinder = function(radius, bar){
-  var geometry = new THREE.CylinderGeometry( radius, radius, params.height, 100);
-  var material = new THREE.MeshLambertMaterial( { color: 0xCE2029 } );
-  var cylinder = new THREE.Mesh( geometry, material );
+let subscribeCylinder = function(radius, bar){
+  let geometry = new THREE.CylinderGeometry( radius, radius, params.height, 100);
+      material = mat_dark;
+      cylinder = new THREE.Mesh( geometry, material );
   cylinder.position.x=2*params.max_radius*(bar+1);
   cylinder.position.y=3;
   cylinder.position.z=params.height*(state.bars[bar].length+1);
@@ -37,12 +42,12 @@ var subscribeCylinder = function(radius, bar){
   state.bars[bar].push( cylinder );
 }
 
-var subscribeAxe = function(bar){
+let subscribeAxe = function(bar){
   const height = params.height*(params.n+2);
   const axis_radius = params.min_radius/3;
-  var geometry = new THREE.CylinderGeometry( axis_radius, axis_radius, height, 100);
-  var material = new THREE.MeshLambertMaterial( { color: 0xFF7F7F } );
-  var cylinder = new THREE.Mesh( geometry, material );
+  let geometry = new THREE.CylinderGeometry( axis_radius, axis_radius, height, 100);
+      material = mat_clear;
+      cylinder = new THREE.Mesh( geometry, material );
   cylinder.position.x = 2*params.max_radius*(bar+1);
   cylinder.position.y = 3;
   cylinder.position.z = height/2;
@@ -50,7 +55,7 @@ var subscribeAxe = function(bar){
   scene.add( cylinder );
 }
 
-var subscribeAxis = function(){
+let subscribeAxis = function(){
   for(let i=0; i<state.bars.length; i++)
     subscribeAxe(i);
 }
@@ -58,7 +63,7 @@ var subscribeAxis = function(){
 subscribeCylinders(0);
 subscribeAxis();
 
-var light = new THREE.PointLight(0xffffff, 1.0);
+let light = new THREE.PointLight(0xffffff, 1.0);
 light.position.set(4, 4, 10);
 
 
@@ -69,9 +74,9 @@ camera.position.y = -3;
 camera.up = new THREE.Vector3(0, 0, 1);
 camera.lookAt(new THREE.Vector3(4, 4, 0));
 
-var t_blacks = Math.PI / 2.0;
-var t_whites = 3.0 * Math.PI / 2.0;
-var anim_time = 0;
+let t_blacks = Math.PI / 2.0;
+    t_whites = 3.0 * Math.PI / 2.0;
+    anim_time = 0;
 
 t = t_whites
 
